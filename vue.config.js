a87c6043b16ad015bdf6727c1e5377929854bb2c
on3dd <onedeadwave@gmail.com>
2020-07-23 22:26:15 +1000

    Minor upd

--- vue.config.js
+++ vue.config.js
@@ -14,27 +14,18 @@ module.exports = {
     extract: process.env.NODE_ENV === 'production',
   },
   configureWebpack: () => ({
-    // 将 entry 指向应用程序的 server / client 文件
     entry: {
       app: `./src/entry-${target}.js`,
       styles: ['./node_modules/@mdi/font/css/materialdesignicons.min.css'],
     },
-    // 对 bundle renderer 提供 source map 支持
     devtool: 'source-map',
     target: TARGET_NODE ? 'node' : 'web',
     node: TARGET_NODE ? undefined : false,
     output: {
       libraryTarget: TARGET_NODE ? 'commonjs2' : undefined,
     },
-    // https://webpack.js.org/configuration/externals/#function
-    // https://github.com/liady/webpack-node-externals
-    // 外置化应用程序依赖模块。可以使服务器构建速度更快，
-    // 并生成较小的 bundle 文件。
     externals: TARGET_NODE
       ? nodeExternals({
-          // 不要外置化 webpack 需要处理的依赖模块。
-          // 你可以在这里添加更多的文件类型。例如，未处理 *.vue 原始文件，
-          // 你还应该将修改 `global`（例如 polyfill）的依赖模块列入白名单
           whitelist: [/\.css$/],
         })
       : undefined,
