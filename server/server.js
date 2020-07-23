b7b911ef25a2de01e5c8e7763f6f8bc7a20b93bf
on3dd <onedeadwave@gmail.com>
2020-07-23 15:55:21 +1000

    Minor upd

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const router = new Router();
 
 const resolve = file => path.resolve(__dirname, file);
 
-// 第 2 步：获得一个createBundleRenderer
 const { createBundleRenderer } = require("vue-server-renderer");
 const bundle = require("../dist/vue-ssr-server-bundle.json");
 const clientManifest = require("../dist/vue-ssr-client-manifest.json");
@@ -17,7 +16,7 @@ const renderer = createBundleRenderer(bundle, {
   clientManifest: clientManifest
 });
 
-function renderToString(context) {
+const renderToString = (context) => {
   return new Promise((resolve, reject) => {
     renderer.renderToString(context, (err, html) => {
       err ? reject(err) : resolve(html);
@@ -25,10 +24,10 @@ function renderToString(context) {
   });
 }
 
-// 第 3 步：添加一个中间件来处理所有请求
 const handleRequest = async (ctx, next) => {
   const url = ctx.path;
   console.log(url);
+
   if (url.includes(".")) {
     console.log(`proxy ${url}`);
     return await send(ctx, url, { root: path.resolve(__dirname, "../dist") });
@@ -39,7 +38,7 @@ const handleRequest = async (ctx, next) => {
     title: "ssr test",
     url
   };
-  // 将 context 数据渲染为 HTML
+
   const html = await renderToString(context);
   ctx.body = html;
 };
--- server/ssr.js
+++ server/ssr.js
@@ -10,7 +10,7 @@ const isDev = process.env.NODE_ENV !== "production";
 const router = isDev ? require("./dev.ssr") : require("./server");
 
 app.use(router.routes()).use(router.allowedMethods());
-// 开放目录
+
 app.use(koaMount("/dist", koaStatic(resolve("../dist"))));
 app.use(koaMount("/public", koaStatic(resolve("../public"))));
 
